48aef64604122bd23d17d695b9e53ca03ab8393d
Chris Meade <chrism@target-technology.co.uk>
2023-09-10 16:37:09 +0100

    tailwind working

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,16 +1,15 @@
+import './styles.css';
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 
 
 const App = () => {
   return (
-    <button >
+    <button className="bg-blue-400" >
       Click to get prime numbers
     </button>
   );
 };
-
-
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader'] },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+
     ],
   },
   resolve: {
